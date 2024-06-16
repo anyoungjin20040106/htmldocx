a823fd6141e8a2b4804d14c5618bddca7d60c696
안영진 <158057603+janyoungjin@users.noreply.github.com>
2024-06-16 19:32:17 +0900

    2장 추가

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
-module HTMLdoc
+module main
 
 go 1.22.2
-
-require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
+	"os"
+	"text/template"
 )
 
+func Warning(w http.ResponseWriter, msg string) {
+	warning, _ := template.ParseFiles("warning.html")
+	data := struct {
+		MSG string
+	}{
+		msg,
+	}
+	warning.Execute(w, data)
+}
 func main() {
-	c := http.FileServer(http.Dir("chapter"))
-	http.Handle("/c/", http.StripPrefix("/c/", c))
+	port := fmt.Sprintf(":%d", rand.Intn(8999)+1000)
+	fileServer := http.FileServer(http.Dir("./chapter"))
+	http.Handle("/c/", http.StripPrefix("/c/", fileServer))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
 	})
-	http.ListenAndServe(":1234", nil)
+
+	http.HandleFunc("/doc", func(w http.ResponseWriter, r *http.Request) {
+		pw := r.FormValue("pw")
+		fmt.Println(r.Method)
+		if r.Method != "POST" {
+			Warning(w, "암호를 입력해주세요")
+		} else if pw != os.Getenv("pw") {
+			Warning(w, "암호가 틀렸습니다")
+		} else {
+			http.ServeFile(w, r, "doc.html")
+		}
+	})
+	log.Println("Server started on " + port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
